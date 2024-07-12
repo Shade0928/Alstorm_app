61dfa972cd8f9c7572341709b5642e4a5a4c8ee1
Anant Raj <mr.shade0928@gmail.com>
2024-07-12 12:37:37 +0530

    The fall of Shadow hunt

--- src/test/java/com/training/oracle/bms/AlstormAppApplicationTests.java
+++ src/test/java/com/training/oracle/bms/AlstormAppApplicationTests.java
@@ -3,11 +3,15 @@ package com.training.oracle.bms;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 @SpringBootTest
 class AlstormAppApplicationTests {
 
 	@Test
-	void contextLoads() {
+	void contextLoads() 
+	{
+	assertEquals(190,200);
 	}
+
 
 }
